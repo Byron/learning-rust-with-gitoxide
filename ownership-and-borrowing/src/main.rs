31128f35d663dbd5b588993f163f2754f5420c99
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-10-12 20:55:16 +0800

    end of session 3

--- ownership-and-borrowing/src/main.rs
+++ ownership-and-borrowing/src/main.rs
@@ -1,12 +1,29 @@
-#[derive(Copy, Clone)]
-struct Foo;
+#![allow(dead_code, unused_variables)]
 
-fn kung_foo(_fighter: Foo) {
+#[derive(Debug, Clone)]
+struct Foo(String, String);
+
+#[derive(Clone)]
+struct Bar {
+    hello: String,
+    world: String,
+}
+
+fn kung_foo(_fighter: Foo) {}
+
+fn bar_foo(Bar { hello, world }: Bar) {
+    ()
 }
 
 fn main() {
-    let x = Foo;
+    let x = Foo("hello".to_string(), "world".to_string());
     kung_foo(x.clone());
-    kung_foo(x);
-}
 
+    let something_descriptive = {
+        let mut y = 42;
+        y += 1;
+        y
+    };
+
+    dbg!({ Foo("hello".to_string(), "scope".to_string()) });
+}
