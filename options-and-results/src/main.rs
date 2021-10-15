afeac498c6421d2407344f65e8ae51ce8dd0079b
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-10-15 22:40:39 +0800

    session 5

--- options-and-results/src/main.rs
+++ options-and-results/src/main.rs
@@ -1,3 +1,23 @@
+#![allow(unused, dead_code)]
+
+fn maybe<T>(x: T) -> Option<T> {
+    if std::time::UNIX_EPOCH.elapsed().unwrap().as_secs() % 2 == 0 {
+        Some(x)
+    } else {
+        None
+    }
+}
+
+fn do_it(value: Option<Foo>) {
+    if let Some(value) = value {
+        drop(value)
+    }
+}
+
+#[derive(Debug, Default)]
+struct Foo(u32);
+
 fn main() {
-    println!("Hello, world!");
+    let flag = true;
+    do_it(flag.then(|| Foo(42)))
 }
