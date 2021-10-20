7805e258d7078c2d85fd0b8d85032e6df3dc859e
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-10-21 07:08:02 +0800

    cleanup

--- ownership-and-borrowing/fail/vec-push.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    let mut v = vec![0];
-    let iter = v.iter();
-    v.push(42);
-    for item in iter {
-        println!("{}", item);
-    }
-}
--- ownership-and-borrowing/tests/ownership.rs
+++
@@ -1,7 +0,0 @@
-#[test]
-fn ownership() {
-    let t = trybuild::TestCases::new();
-    t.compile_fail("fail/vec-push.rs");
-    t.pass("pass/borrow-mut-int.rs");
-    t.pass("pass/copy-clone-unit-struct.rs");
-}
