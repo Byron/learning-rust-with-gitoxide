42c707a95211d318c0ec1766020a25edb34103c3
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-10-22 07:14:31 +0800

    session 7

--- options-and-results/src/main.rs
+++ options-and-results/src/main.rs
@@ -1,29 +1,15 @@
 #![allow(unused, dead_code)]
 
-enum MyOption<T> {
-    Some(T),
-    None,
+fn add_one_safely(v: u8) -> Result<u8, &'static str> {
+    v.checked_add(1).ok_or("overflow")
 }
 
-impl<T> MyOption<T> {
-    fn take_map<U>(&mut self, f: impl FnOnce(T) -> U) -> Option<U> {
-        match self.take() {
-            MyOption::Some(v) => Some(f(v)),
-            MyOption::None => None,
-        }
-    }
-
-    fn take(&mut self) -> MyOption<T> {
-        std::mem::replace(self, MyOption::None)
-    }
-}
-
-fn change(mut v: Option<&mut String>) {
-    v.map(|v: &mut String| *v = String::from("changed"));
-}
-
-fn main() {
-    let mut v = vec![String::from("hello")];
-    change(v.get_mut(0));
-    println!("{:?}", v.get(0));
+fn main() -> Result<(), &'static str> {
+    std::env::args()
+        .is_empty()
+        .then(|| "need two args")
+        .map(Err)
+        .transpose()?;
+    println!("{}", add_one_safely(255)?);
+    Ok(())
 }
