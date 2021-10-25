ce18d75780f01f1168d4e8b81c23fcb7aff15b17
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-10-26 07:55:40 +0800

    session 8 part 1

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,1 +1,26 @@
-fn main() {}
+#[derive(Debug)]
+enum OutOfBoundsError {
+    Overflow,
+    Underflow,
+}
+
+struct Adder<'a>(&'a i8);
+
+impl<'a> Adder<'a> {
+    fn add_one(&self) -> Result<i8, OutOfBoundsError> {
+        self.0.checked_add(1).ok_or(OutOfBoundsError::Overflow)
+    }
+
+    fn sub_one(&self) -> Result<i8, OutOfBoundsError> {
+        self.0.checked_sub(1).ok_or(OutOfBoundsError::Underflow)
+    }
+}
+
+fn main() -> Result<(), ()> {
+    match Adder(&126).add_one() {
+        Ok(v) => {}
+        Err(v) => return,
+    }
+    Adder(&-128).sub_one().unwrap();
+    Ok(())
+}
