182371de381b5fb55965ec48dda0a264ebf3afaa
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-10-29 21:38:21 +0800

    session 8 part 2

--- errors/src/main.rs
+++ errors/src/main.rs
@@ -1,7 +1,25 @@
+use anyhow::Context;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+
 #[derive(Debug)]
 enum OutOfBoundsError {
     Overflow,
     Underflow,
+}
+
+impl Display for OutOfBoundsError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+
+impl Error for OutOfBoundsError {}
+
+impl From<OutOfBoundsError> for String {
+    fn from(v: OutOfBoundsError) -> Self {
+        format!("{:?}", v)
+    }
 }
 
 struct Adder<'a>(&'a i8);
@@ -16,11 +34,20 @@ impl<'a> Adder<'a> {
     }
 }
 
-fn main() -> Result<(), ()> {
-    match Adder(&126).add_one() {
-        Ok(v) => {}
-        Err(v) => return,
-    }
-    Adder(&-128).sub_one().unwrap();
+fn always_err() -> anyhow::Result<()> {
+    // anyhow::bail!("hello, failure")
+    Err(anyhow::anyhow!("hello too"))
+}
+
+fn do_it() -> anyhow::Result<()> {
+    let v = Adder(&126).add_one()?;
+    always_err().context("additional information")?;
+    println!("{}", v);
+    Adder(&-128).sub_one()?;
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    do_it()?;
     Ok(())
 }
