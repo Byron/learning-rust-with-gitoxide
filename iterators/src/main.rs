8edcbfcadbf50228e2200898861442556d96376d
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-11-08 19:14:33 +0800

    session 10

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
-    let v = [1, 2, 3];
-    for i in 0..v.len() {
-        println!("{}", v[i]);
-    }
-    println!("Hello, iterators!");
+    let mut v = [String::from("1"), "2".into(), "3".into()];
+    let mut iter = v.iter();
+    let mut iter_ref = iter.by_ref().filter(|s| *s == "2");
+    dbg!(iter_ref.next());
+    dbg!(iter.next());
 }
