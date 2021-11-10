815ffa19345eaea4763906f4dbea7c8d87c9b3e9
Sebastian Thiel <sebastian.thiel@icloud.com>
2021-11-10 19:27:45 +0800

    session 11

--- iterators/src/main.rs
+++ iterators/src/main.rs
@@ -2,11 +2,7 @@ use std::string::FromUtf8Error;
 
 fn main() -> Result<(), FromUtf8Error> {
     let v = [&b"1"[..], &*b"22", b"1"];
-    for s in v
-        .into_iter()
-        .filter_map(|v| String::from_utf8(v.into()).ok())
-    {
-        println!("{}", s)
-    }
+    v.into_iter()
+        .filter_map(|v| String::from_utf8(v.into()).ok());
     Ok(())
 }
